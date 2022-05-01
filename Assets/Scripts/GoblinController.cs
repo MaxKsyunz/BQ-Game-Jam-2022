365bf6e0e6d6648edf5e7db8aa5e9e74c6a0d6a3
Sertay Sener <sertaysener@Sertays-MBP.hitronhub.home>
2022-04-30 18:43:08 -0700

    update goblin

--- Assets/Scripts/GoblinController.cs
+++ Assets/Scripts/GoblinController.cs
@@ -4,49 +4,62 @@ using UnityEngine;
 
 public class GoblinController : MonoBehaviour
 {
-    public float speed = 3f;
-    public Transform[] waypoints;
-    bool loop = true;
-    //float pauseDuration = 0;
+    // Level specific waypoint list.
+    // Don't forget to drag and drop in the editor!
+    public List<GameObject> defaultWaypoints;
 
-    float curTime;
-    int currentWaypoint = 0;
+    public float speed;
+    public const float  patrolSpeed = 2f;
+    LinkedList<GameObject> curPath;
+    public float sightRadius = 5f;
 
+    GameObject currentTarget;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        speed = patrolSpeed;
+        curPath = new LinkedList<GameObject>();
+        loadDefaultPath();
+        currentTarget = curPath.First.Value;
     }
 
-    // Update is called once per frame
+    private void loadDefaultPath()
+    {
+        for (int i = defaultWaypoints.Count - 1; i > 0; i--)
+        {
+            curPath.AddLast(defaultWaypoints[i]);
+        }
+    }
+
     void Update()
     {
-        if (currentWaypoint < waypoints.Length)
-        {
-            Patrol();
-        }
-        else
-        {
-            if (loop)
-            {
-                currentWaypoint = 0;
-            }
-        }
+        WalkAround();
     }
 
-    void Patrol()
+    void WalkAround()
     {
-
-        Vector3 target = waypoints[currentWaypoint].position;
         var step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target, step);
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget.transform.position, step);
 
-        if (transform.position == target)
+        if (transform.position == currentTarget.transform.position)
         {
-            currentWaypoint++;
+            curPath.RemoveFirst();
+            if (curPath.Count == 0)
+            {
+                loadDefaultPath();
+            }
+
+            currentTarget = curPath.First.Value;
         }
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, sightRadius);
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+    }
 }
